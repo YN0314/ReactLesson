27ce08bbddeb8b787c1d27e46f5e18ae038731a3
nakagawa <nakagawa@brave-heart.co.jp>
2021-10-26 18:22:14 +0900

    add library ky, axios

--- src/app/src/page/one.js
+++ src/app/src/page/one.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './../logo.svg';
 import Two from './two';
 
 class One extends React.Component {
@@ -8,6 +9,7 @@ class One extends React.Component {
             <div>
                 test_one<br/>
                 <Link to='/two'>to_two</Link>
+                <img src={logo} />
             </div>
         )
     }
